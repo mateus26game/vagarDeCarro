2803a3332eaf7898d0b6e6c027e5438d9b65d88e
mateus26game <135272595+mateus26game@users.noreply.github.com>
2024-01-11 01:19:23 -0300

    Update nova.cs

--- nova.cs
+++ nova.cs
@@ -35,25 +35,22 @@ namespace visual_studui
     }
 
     static void FicharEstacionamento() {
-        // Obtém o horário atual do sistema
+      
         DateTime horarioAtual = DateTime.Now;
 
-        // Imprime o horário no formato padrão
+     
         Console.WriteLine("Fichando estacionamento... sua hora: " + horarioAtual);
-        // Lógica para fichar estacionamento
+        
     }
 
     static void PagarEstacionamento() {
-        // Pergunta a hora que o usuário sai do estacionamento
         Console.WriteLine("Digite a hora que você está saindo do estacionamento (formato HH:mm): ");
         string horaSaidaString = Console.ReadLine();
 
-        // Tenta converter a hora de saída para um objeto DateTime
         if (DateTime.TryParseExact(horaSaidaString, "HH:mm", null, System.Globalization.DateTimeStyles.None, out DateTime horaSaida)) {
-            // Calcula a diferença em minutos
+          
             TimeSpan diferenca = horaSaida - DateTime.Now;
 
-            // Compara com 15 minutos
             if (diferenca.TotalMinutes <= 15) {
                 Console.WriteLine("Você não precisa pagar. Menos de 15 minutos no estacionamento.");
             } else {
